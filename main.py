251248c8caff25b3d4f2adc9e31e163c28626ce0
AkagiYuu <khanghuynh0245@gmail.com>
2022-09-16 23:08:09 +0700

    Update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from vex import (
-    Brain, Controller, Motor,
-    Ports, BrakeType, FORWARD, PERCENT, REVERSE, SECONDS
+    Brain, Controller, Motor, Ports,
+    FORWARD, PERCENT, REVERSE, SECONDS, DEGREES
 )
 from drivetrain import Drivetrain
 
@@ -8,61 +8,61 @@ from drivetrain import Drivetrain
 brain = Brain()
 
 controller = Controller()
-# nếu joystick không di chuyển hơn 3% thì coi như joystick không di chuyển
 controller.set_deadband(3)
 
 left_motor = Motor(Ports.PORT1)
 right_motor = Motor(Ports.PORT6, True)
 driver = Drivetrain(left_motor, right_motor)
 
-tension_motor = Motor(Ports.PORT5)
+spin_motor = Motor(Ports.PORT5)
 arm_motor = Motor(Ports.PORT10)
-shooting_motor = Motor(Ports.PORT7)
+shoot_motor = Motor(Ports.PORT7)
 # endregion
 
 # region Helpers
-# def lower_or_raise_arm_by_controller(arm_motor_velocity_percent=100):
-#     if controller.buttonLDown.pressing() or controller.buttonRDown.pressing():
-#         arm_motor.spin(REVERSE, arm_motor_velocity_percent, PERCENT)
+def joystick_position_to_angle(position):
+    if position < - 100 or position > 100:
+        return 0
+    return position * 9 / 5
 
-#     elif controller.buttonLUp.pressing() or controller.buttonRUp.pressing():
-#         arm_motor.spin(FORWARD, arm_motor_velocity_percent, PERCENT)
 
-#     else:
-#         arm_motor.stop(BrakeType.HOLD)
-
-# def close_or_open_claw_by_controller(claw_motor_velocity_percent=100):
-#     if controller.buttonEDown.pressing():
-#         claw_motor.spin(REVERSE, claw_motor_velocity_percent, PERCENT)
-
-#     elif controller.buttonEUp.pressing():
-#         claw_motor.spin(FORWARD, claw_motor_velocity_percent, PERCENT)
-
-#     else:
-#         claw_motor.stop(BrakeType.HOLD)
-
-def drive_by_controller():
+def drive_using_controller():
     drive_power = controller.axisA.position()
     turn_power = controller.axisB.position()
 
     driver.arcade(drive_power, turn_power)
 
-def move_arm():
-    pass
+
+def move_arm_using_controller():
+    angle = joystick_position_to_angle(controller.axisC.position())
+    arm_motor.spin_for(FORWARD, angle, DEGREES, None, PERCENT, False)
 
 
-
-def spin():
+def spin(velocity=100):
     if controller.buttonLUp.pressing():
+        spin_motor.spin(REVERSE, velocity, PERCENT)
         return
     if controller.buttonLDown.pressing():
+        spin_motor.spin(FORWARD, velocity, PERCENT)
         return
 
-def ...
+
+def shoot(velocity=100):
+    if controller.buttonRUp.pressing():
+        shoot_motor.spin(FORWARD, velocity, PERCENT)
+        return
+    if controller.buttonRDown.pressing():
+        shoot_motor.spin(REVERSE, velocity, PERCENT)
+        return
 # endregion
 
+
 def main():
-    pass
+    drive_using_controller()
+    move_arm_using_controller()
+    spin()
+    shoot()
+
 
 while True:
     main()
