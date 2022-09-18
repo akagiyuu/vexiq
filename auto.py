29325c1ea00d067437b2c3edc4d8f76de3b1a7d8
AkagiYuu <khanghuynh0245@gmail.com>
2022-09-18 18:03:22 +0700

    Update

--- auto.py
+++ auto.py
@@ -1,4 +1,5 @@
 from sunau import AUDIO_FILE_ENCODING_LINEAR_16
+from tkinter import W
 import vex
 import sys
 import drivetrain
@@ -18,62 +19,23 @@ GRID_SIZE = 315
 STEP = 20
 BRIGHTNESS_THRESHOLD = 6
 ANGLE_FOR_BLUE_DISPENSER = 60
-
-
 # endregion
+
+# region Enums
+
 
 class MoveType():
     Forward = FORWARD
     Reverse = REVERSE
     Turn = 2
-    Spin = 3
-    Shoot = 4
-    Arm = 5
+    Shoot = 3
+
 
 class DispenserType():
     Purple = 0
     Blue = 1
     Yellow = 2
-
-
-class AutoDrive:
-    move_sequence = [
-        [[MoveType.Forward, 3], [MoveType.Turn, 90], [MoveType.Forward, 2], [MoveType.Spin, 200], [MoveType.Reverse, 1],
-         [MoveType.Turn, 90], [MoveType.Forward, 1], [MoveType.Arm], [MoveType.Reverse, 2], [MoveType.Shoot, 2000]],
-
-    ]
-
-    def move(direction, number_of_grid):
-        grid_pass = 0
-        while grid_pass < number_of_grid:
-            driver.drive_for(direction, STEP)
-            if color_sensor.grayscale() > BRIGHTNESS_THRESHOLD:
-                grid_pass += 1
-
-    def start(this):
-        this.get_dispensers(0)
-
-    def get_disk_from_dispenser(type: DispenserType):
-        if type == DispenserType.Purple:
-            pass
-        elif type == DispenserType.Blue:
-            arm_motor.spin_for(REVERSE, ANGLE_FOR_BLUE_DISPENSER)
-        elif type == DispenserType.Yellow:
-            pass
-
-    def get_dispensers(this, index):
-        for move in this.move_sequence[index]:
-            if move[0] == MoveType.Forward or move[0] == MoveType.Reverse:
-                move(move[0], move[1])
-            elif move[0] == MoveType.Turn:
-                driver.turn_for(FORWARD, move[1])
-            elif move[0] == MoveType.Spin:
-                pass
-            elif move[0] == MoveType.Shoot:
-                shoot()
-            elif move[0] == MoveType.Arm:
-                arm_motor.spin_for(REVERSE, 60)
-
+# endregion
 
 
 # region Initialize
@@ -91,10 +53,46 @@ color_sensor = Colorsensor(Ports.PORT8)
 # endregion
 
 
-# region Helpers
+class AutoDrive:
+    move_sequence = [
+        [MoveType.Forward, 3],
+        [MoveType.Turn, 90],
+        [MoveType.Forward, 2],
+        [MoveType.Reverse, 1],
+        [MoveType.Turn, 90],
+        [MoveType.Forward, 1],
+        [MoveType.Reverse, 2],
+        [MoveType.Shoot, 2000]
+    ]
 
-# endregion
+    def move(direction, number_of_grid):
+        grid_pass = 0
+        while grid_pass < number_of_grid:
+            driver.drive_for(direction, STEP)
+            if color_sensor.grayscale() > BRIGHTNESS_THRESHOLD:
+                grid_pass += 1
+
+    def get_disk_from_dispenser(type: DispenserType):
+        if type == DispenserType.Purple:
+            pass
+        elif type == DispenserType.Blue:
+            arm_motor.spin_for(REVERSE, ANGLE_FOR_BLUE_DISPENSER)
+        elif type == DispenserType.Yellow:
+            pass
+
+    def start_moving(this):
+        for move in this.move_sequence:
+            if move[0] == MoveType.Forward or move[0] == MoveType.Reverse:
+                move(move[0], move[1])
+            elif move[0] == MoveType.Turn:
+                driver.turn_for(FORWARD, move[1])
+            elif move[0] == MoveType.Spin:
+                pass
+            elif move[0] == MoveType.Shoot:
+                shoot()
+            elif move[0] == MoveType.Arm:
+                arm_motor.spin_for(REVERSE, 60)
 
 
 auto_drive = AutoDrive()
-auto_drive.start()
+auto_drive.start
