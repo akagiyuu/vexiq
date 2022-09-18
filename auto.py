26f9f0c592697b7f1f5475ae9ffa7d31ccf556fe
AkagiYuu <khanghuynh0245@gmail.com>
2022-09-18 19:26:42 +0700

    Update

--- auto.py
+++ auto.py
@@ -5,17 +5,18 @@ import sys
 import drivetrain
 import smartdrive
 import vision
+import math
 
 from vex import (
 
     Brain, Motor, Ports, Colorsensor,
-    FORWARD, PERCENT, REVERSE, SECONDS, DEGREES
+    FORWARD, PERCENT, REVERSE, SECONDS, DEGREES, INCHES
 )
 from drivetrain import Drivetrain
 
 # region Constant
 ANGLE_TO_PREPARE_STATE = 180
-GRID_SIZE = 315
+GRID_SIZE = 12  # 315
 STEP = 20
 BRIGHTNESS_THRESHOLD = 6
 ANGLE_FOR_BLUE_DISPENSER = 60
@@ -29,6 +30,7 @@ class MoveType():
     Reverse = REVERSE
     Turn = 2
     Shoot = 3
+    GetDisk = 4
 
 
 class DispenserType():
@@ -43,7 +45,7 @@ brain = Brain()
 
 left_motor = Motor(Ports.PORT12)
 right_motor = Motor(Ports.PORT7, True)
-driver = Drivetrain(left_motor, right_motor)
+driver = Drivetrain(left_motor, right_motor, 8, 11, INCHES)
 
 spin_motor = Motor(Ports.PORT10)
 arm_motor = Motor(Ports.PORT9)
@@ -55,22 +57,42 @@ color_sensor = Colorsensor(Ports.PORT8)
 
 class AutoDrive:
     move_sequence = [
-        [MoveType.Forward, 3],
-        [MoveType.Turn, 90],
-        [MoveType.Forward, 2],
+        # [MoveType.Forward, 1],
+        # [MoveType.Turn, 45],
+        # [MoveType.Forward, math.sqrt(2)],
+        # [MoveType.Turn, 45],
+        # [MoveType.Forward, 0.2],
+        # [MoveType.GetDisk, DispenserType.Purple]
+        # [MoveType.Reverse, 0.2],
+        # [MoveType.Turn, 90],
+        # [MoveType.GetDisk, DispenserType.Blue]
+        # [MoveType.Turn, - 30]
+        # [MoveType.Reverse, math.sqrt(5) / 2],
+        # [MoveType.Turn, 30]
+        # [MoveType.Reverse, (1 + math.sqrt(2))]
+        # [MoveType.Shoot, 2000]
+        [MoveType.Forward, 2.06], #Move until reach yellow dispenser
+        [MoveType.GetDisk, DispenserType.Yellow],
         [MoveType.Reverse, 1],
+        [MoveType.Turn, 135],
+        [MoveType.Reverse, math.sqrt(2)],
+        [MoveType.Turn, 45],
+        [MoveType.Reverse, 2]
+        [MoveType.Shoot, 2],
+        [MoveType.Forward, 1.5],
         [MoveType.Turn, 90],
-        [MoveType.Forward, 1],
-        [MoveType.Reverse, 2],
-        [MoveType.Shoot, 2000]
+        [MoveType.Forward, 1.5],
     ]
 
     def move(direction, number_of_grid):
-        grid_pass = 0
-        while grid_pass < number_of_grid:
-            driver.drive_for(direction, STEP)
-            if color_sensor.grayscale() > BRIGHTNESS_THRESHOLD:
-                grid_pass += 1
+        driver.drive_for(direction, GRID_SIZE * number_of_grid,
+                         INCHES, None, PERCENT, False)
+        # grid_pass = 0
+        # grids = math.floor(number_of_grid)
+
+        # while not driver.is_done() and grid_pass < grids:
+        #     if color_sensor.grayscale() <= BRIGHTNESS_THRESHOLD:
+        #         grid_pass += 1
 
     def get_disk_from_dispenser(type: DispenserType):
         if type == DispenserType.Purple:
