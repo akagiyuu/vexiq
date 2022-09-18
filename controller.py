df444281ae2c90a031f70ba4216e7ee1a954250c
AkagiYuu <khanghuynh0245@gmail.com>
2022-09-18 21:15:16 +0700

    Update

--- controller.py
+++ controller.py
@@ -18,59 +18,6 @@ DefaultVelocity = 80
 DeadBand = 10
 # endregion
 
-class Controller_Extent(Controller):
-    def __init__(this, driver: Drivetrain, spin_motor: Motor, shoot_motor: Motor, arm_motor: Motor):
-        Controller.__init__(this)
-        this.Angle = 20
-        this.Velocity = 100
-
-        this.driver = driver
-        this.spin_motor = spin_motor
-        this.shoot_motor = shoot_motor
-        this.arm_motor = arm_motor
-
-    def drive(this):
-        drive_power = this.axisA.position()
-        turn_power = this.axisC.position()
-
-        this.driver.arcade(drive_power, turn_power)
-
-    def move_arm(this):
-        if this.buttonFUp.pressing():
-            this.arm_motor.spin_for(REVERSE, DefaultAngle)
-            return
-        if this.buttonFDown.pressing():
-            this.arm_motor.spin_for(FORWARD, DefaultAngle)
-            return
-
-    def spin(this):
-        if this.buttonLUp.pressing():
-            this.spin_motor.spin(REVERSE, DefaultVelocity)
-            return
-        if this.buttonLDown.pressing():
-            this.spin_motor.stop(vex.BrakeType.HOLD)
-            return
-
-    def shoot(this):
-        if this.buttonRUp.pressing():
-            this.shoot_motor.spin(FORWARD, DefaultVelocity)
-            return
-        if this.buttonRDown.pressing():
-            this.shoot_motor.stop(vex.BrakeType.COAST)
-            return
-
-    def shooting_prepare(this):
-        if this.buttonEUp.pressing():
-            this.shoot_motor.spin_for(FORWARD, AngleToPrepareState)
-
-    def detect_input(this):
-        this.drive()
-        this.move_arm()
-        this.spin()
-        this.shoot()
-        this.shooting_prepare()
-
-
 # region Initialize
 brain = Brain()
 
@@ -81,10 +28,60 @@ driver = Drivetrain(left_motor, right_motor)
 spin_motor = Motor(Ports.PORT10)
 arm_motor = Motor(Ports.PORT9)
 shoot_motor = Motor(Ports.PORT11)
-
-controller = Controller_Extent(driver, spin_motor, shoot_motor, arm_motor)
-controller.set_deadband(DeadBand)
 # endregion
+
+
+class Controller_Extent(Controller):
+    def __init__(this):
+        Controller.__init__(this)
+        this.Angle = 20
+        this.Velocity = 100
+
+    def drive(this):
+        drive_power = this.axisA.position()
+        turn_power = this.axisC.position()
+
+        driver.arcade(drive_power, turn_power)
+
+    def move_arm(this):
+        if this.buttonFUp.pressing():
+            arm_motor.spin_for(REVERSE, DefaultAngle)
+            return
+        if this.buttonFDown.pressing():
+            arm_motor.spin_for(FORWARD, DefaultAngle)
+            return
+
+    def spin(this):
+        if this.buttonLUp.pressing():
+            spin_motor.spin(REVERSE, DefaultVelocity)
+            return
+        if this.buttonLDown.pressing():
+            spin_motor.stop(vex.BrakeType.HOLD)
+            return
+
+    def shoot(this):
+        if this.buttonRUp.pressing():
+            shoot_motor.spin(FORWARD, DefaultVelocity)
+            return
+        if this.buttonRDown.pressing():
+            shoot_motor.stop(vex.BrakeType.COAST)
+            return
+
+    def shooting_prepare(this):
+        if this.buttonEUp.pressing():
+            shoot_motor.spin_for(FORWARD, AngleToPrepareState)
+
+    def detect_input(this):
+        this.drive()
+        this.move_arm()
+        this.spin()
+        this.shoot()
+        this.shooting_prepare()
+
+
+controller = Controller_Extent()
+controller.set_deadband(DeadBand)
+
 
 while True:
     controller.detect_input()
